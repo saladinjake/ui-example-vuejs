157b3ce75a609c47934fe43f31b40a6bf6d7f23f
saladinjake <juwavictor@gmail.com>
2022-08-03 12:15:05 +0100

    starter ui for test sample

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,71 @@
 <template>
   <v-app>
-    do something ui awesome
-
+    <navigation :color="color" :flat="flat" />
+    <v-main class="pt-0">
+    </v-main>
+    
   </v-app>
 </template>
 
 <style scoped>
 .v-main {
-  background: #eaeaea;
-
+ background:#eaeaea;
 }
 </style>
 
 <script>
+import navigation from "./components/Navigation";
+
 
 export default {
   name: "App",
 
   components: {
+    navigation,
+    foote,
+    home,
+    about,
+    download,
+    pricing,
+    contact,
   },
 
   data: () => ({
-    
+    fab: null,
+    color: "",
+    flat: null,
   }),
 
   created() {
-    
+    const top = window.pageYOffset || 0;
+    if (top <= 60) {
+      this.color = "transparent";
+      this.flat = true;
+    }
   },
 
   watch: {
-    
+    fab(value) {
+      if (value) {
+        this.color = "#fff";
+        //this.color = "secondary";
+        this.flat = false;
+      } else {
+        this.color = "#fff";
+        this.flat = true;
+      }
+    },
   },
 
   methods: {
-    
+    onScroll(e) {
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 60;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
   },
 };
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      temporary
+      
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="@/assets/img/logo.jpg" alt="Logo" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="title">LeapSail</v-list-item-title>
+            
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list dense>
+        <v-list-item
+          v-for="([icon, text, link], i) in items"
+          :key="i"
+          link
+          @click="$vuetify.goTo(link)"
+        >
+          <v-list-item-icon class="justify-center">
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="subtitile-1">{{
+              text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      :color="color"
+      
+      
+      class="px-15 background-nav"
+      :class="{ expand: flat }"
+    >
+      <v-toolbar-title>
+        <v-img src="@/assets/img/logo.jpg" max-width="50px" />
+      </v-toolbar-title>
+      <v-spacer />
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="mr-4"
+        v-if="isXs"
+      />
+      <div v-else>
+        <v-btn text @click="$vuetify.goTo('#hero')">
+          <span class="mr-2">Why Leapsail</span>
+        </v-btn>
+        <v-btn text @click="$vuetify.goTo('#features')">
+          <span class="mr-2">Products</span>
+        </v-btn>
+        <v-btn text @click="$vuetify.goTo('#download')">
+          <span class="mr-2">Pricing</span>
+        </v-btn>
+        <v-btn text @click="$vuetify.goTo('#pricing')">
+          <span class="mr-2">Learn</span>
+        </v-btn>
+
+         <v-btn text @click="$vuetify.goTo('#pricing')">
+          <span class="mr-2">Contact</span>
+        </v-btn>
+
+         <v-btn text @click="$vuetify.goTo('#pricing')">
+          <span class="mr-2">Support</span>
+        </v-btn>
+
+        <v-btn rounded outlined text @click="$vuetify.goTo('#contact')">
+          <span class="mr-2">Login</span>
+        </v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<style scoped>
+.v-toolbar {
+  transition: 0.6s;
+}
+
+.expand {
+  height: 80px !important;
+  padding-top: 10px;
+  background-color:#fff;
+}
+.background-nav{
+  background:#fff;
+}
+</style>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    isXs: false,
+    items: [
+      ["mdi-home-outline", "Why leapsail", "#hero"],
+      ["mdi-information-outline", "Products", "#features"],
+      ["mdi-download-box-outline", "Pricing", "#download"],
+      ["mdi-currency-usd", "Learn", "#pricing"],
+      ["mdi-email-outline", "Contact", "#contact"],
+      ["mdi-email-outline", "Support", "#contact"],
+      ["mdi-email-outline", "Login", "#contact"],
+    ],
+  }),
+  props: {
+    color: String,
+    flat: Boolean,
+  },
+  methods: {
+    onResize() {
+      this.isXs = window.innerWidth < 850;
+    },
+  },
+
+  watch: {
+    isXs(value) {
+      if (!value) {
+        if (this.drawer) {
+          this.drawer = false;
+        }
+      }
+    },
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener("resize", this.onResize, { passive: true });
+  },
+};
+</script>
